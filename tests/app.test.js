7125c3edb84feeb4547132288b2c978215f5731d
sformichella <sformichella@gmail.com>
2020-12-08 17:36:45 -0800

    get theorems test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,18 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+const calculusTheorem = {
+  id: "1",
+  title: "Fundamental Theorem of Calculus",
+  description: "Describes a fundamental relationship betweem derivatives and anti-derivatives",
+  url: "https://en.wikipedia.org/wiki/Fundamental_theorem_of_calculus"
+}
+
 describe('Theorem routes', () => {
-  it('should return a theorem', async () => {
-    return request(app)
-      .get('/')
-      .then(res => expect(res.body.message).toEqual('Hi'))
+  it('should return some theorems', async () => {
+    const response = await request(app)
+      .get('/theorems');
+
+    expect(response.body).toEqual(calculusTheorem);
   })
 })

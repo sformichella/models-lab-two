3a47c44f8b46d2003bbe24b5443c733823663ca9
sformichella <sformichella@gmail.com>
2020-12-08 18:05:06 -0800

    create make a theorem test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,18 +1,35 @@
 const request = require('supertest');
+const fileSystem = require('fs').promises;
+const pool = require('../../postgres-models-lab/lib/utils/pool');
 const app = require('../lib/app');
 
-const calculusTheorem = {
-  id: "1",
-  title: "Fundamental Theorem of Calculus",
-  description: "Describes a fundamental relationship betweem derivatives and anti-derivatives",
-  url: "https://en.wikipedia.org/wiki/Fundamental_theorem_of_calculus"
-}
 
 describe('Theorem routes', () => {
-  it('should return some theorems', async () => {
-    const response = await request(app)
-      .get('/theorems');
 
-    expect(response.body).toEqual(calculusTheorem);
+  beforeAll(async () => {
+    const setupSQL = await fileSystem.readFile('./sql/setup.sql', 'utf-8');
+
+    await pool.query(setupSQL);
+  })
+
+
+
+  it('should create a theorem and return it', async () => {
+    const theorem = {
+      title: "Fundamental Theorem of Calculus",
+      description: "Describes a fundamental relationship betweem derivatives and anti-derivatives",
+      url: "https://en.wikipedia.org/wiki/Fundamental_theorem_of_calculus"
+    }
+
+    const response = await request(app)
+      .post('/make/theorem')
+      .send(theorem);
+
+    expect(response.body).toEqual({
+      id: "1",
+      title: "Fundamental Theorem of Calculus",
+      description: "Describes a fundamental relationship betweem derivatives and anti-derivatives",
+      url: "https://en.wikipedia.org/wiki/Fundamental_theorem_of_calculus"
+    })
   })
 })
